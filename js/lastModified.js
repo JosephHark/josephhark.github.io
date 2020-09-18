3af7a93155f6b6f8e55fcde0cf9bd2eb0b1dbf12
Joseph Hark <har17033@byui.edu>
2020-09-18 15:20:51 -0400

    final commit

--- js/lastModified.js
+++ js/lastModified.js
@@ -1,1 +1,30 @@
 document.getElementById('lastModified').innerHTML = document.lastModified;
+let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "Ocotober",
+    "Novemeber",
+    "Decemeber"
+];
+let d = new Date();
+let day = weekdays[d.getDate];
+let monthName = month[d.getMonth];
+let year = d.getFullYear
+let date = day + "," + monthName + " " + d.getDate() + "," + year;
+document.getElementById("currentdate").textContent = date;
