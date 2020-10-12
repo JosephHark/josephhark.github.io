92b5c9e06b75fa96572be82d0ea0daff4222bd8f
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-10-12 10:00:21 -0400

    commit

--- lesson5/picnic.js
+++ lesson5/picnic.js
@@ -1,3 +1,13 @@
-
+let week = ["Sunday",
+    "Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday",];
+let d = new Date();
+let dayname = week[d.getDay()];
+if (dayname == "Friday"){
 var banner = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
 document.getElementById("picnic").textContent = banner;
+}
