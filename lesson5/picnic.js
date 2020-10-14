5be2a738eb3320f21938e677749d1ddcb4c38f84
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-10-14 09:34:44 -0400

    commit

--- lesson5/picnic.js
+++ lesson5/picnic.js
@@ -1,13 +1,14 @@
 let week = ["Sunday",
     "Monday",
-"Tuesday",
-"Wednesday",
-"Thursday",
-"Friday",
-"Saturday",];
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
 let d = new Date();
 let dayname = week[d.getDay()];
-if (dayname == "Friday"){
-var banner = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-document.getElementById("picnic").textContent = banner;
+if (dayname == "Friday") {
+    var banner = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    document.getElementById("picnic").textContent = banner;
 }
--- lesson5/town5.js
+++ lesson5/town5.js
@@ -1,22 +1,24 @@
 let week = ["Sunday",
     "Monday",
-"Tuesday",
-"Wednesday",
-"Thursday",
-"Friday",
-"Saturday",];
-let monthNames = [ "January",
-"February",
-"March",
-"April",
-"May",
-"June",
-"July",
-"August",
-"September",
-"October",
-"November",
-"December"];
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+let monthNames = ["January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 
 let d = new Date();
 let dayname = week[d.getDay()];
@@ -24,9 +26,3 @@ let month = monthNames[d.getMonth()];
 let year = d.getFullYear();
 let fulldate = dayname + " , " + month + " " + d.getDate() + " , " + year;
 document.getElementById("currentDate").textContent = fulldate;
-
-if (week == 5){
-    display: block;
-    var banner = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-document.getElementById("picnic").textContent = banner;
-}
