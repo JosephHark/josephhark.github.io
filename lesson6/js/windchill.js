8986946978c4f3360ec1893f2ef54ec73faca388
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-10-24 12:58:06 -0400

    commit

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,16 +1,17 @@
 let t = 69;
 let speed = 15;
 /*Calculates windchill*/
-function buildWC(speed, t);{
-    let feelTemp = document.getElementById('chill');
+function buildWC(speed, t){
+let feelTemp = document.getElementById('chill');
 
-    //compute
-    let wc = 35.74 + (0.6215 * t) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * t * Math.pow(speed, 0.16));
-    console.log(wc);
-    wc=Math.floor(wc);
+let wc = 35.74 + (0.6215 * t) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * t * Math.pow(speed, 0.16));
+console.log(wc);
+wc=Math.floor(wc);
+//compute
 
-    //if windchill is greater than temp return windchill
-    wc = (wc > temp) ? temp:wc;
-    console.log(wc);
-    feelTemp.innerText = wc;
+//if windchill is greater than temp return windchill
+wc = (wc > temp) ? temp:wc;
+console.log(wc);
+feelTemp.innerText = wc;
 }
+buildWC(speed, t);
