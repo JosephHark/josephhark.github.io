abf02054d9c3702858be63e99fa2e5d33f6958d4
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-10-26 09:07:32 -0400

    c

--- Lesson 7/app.js
+++ Lesson 7/app.js
@@ -1,4 +1,14 @@
-if('IntersectionObserver' in window) {
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+  if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
       items.forEach((item) => {
         if(item.isIntersecting) {
