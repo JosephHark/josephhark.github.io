ae9593702b591ce2f06213f289b92caec4726059
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-10-28 18:44:25 -0400

    commit

--- Lesson 7/app.js
+++ Lesson 7/app.js
@@ -1,10 +1,26 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px"
+}
 const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
-};
-imagesToLoad.forEach((img) => {
-  loadImages(img);
-});
+    image.setAttribute('src', ('data-src'));
+    image.onload = () => {
+      image.removeAttribute('data-src');
+    };
+    if ('IntersectionObserver' in window) {
+      const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+
+        });
+      }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+      imgObserver.observe(img);
+    });
+  }
+  else{
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
