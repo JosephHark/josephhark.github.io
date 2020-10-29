e99590d1be8a2ed0640574ff53452f6e5fe56b92
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-10-28 22:34:32 -0400

    commit

--- Lesson 7/app.js
+++ Lesson 7/app.js
@@ -1,26 +1,28 @@
-const imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+// console.log(imagesToLoad);
 const imgOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px 50px 0px"
+  threshold: .5,
 }
 const loadImages = (image) => {
-    image.setAttribute('src', ('data-src'));
-    image.onload = () => {
-      image.removeAttribute('data-src');
-    };
-    if ('IntersectionObserver' in window) {
-      const imgObserver = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-
-        });
-      }, imgOptions);
-
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+  if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, imgObserver) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          imgObserver.unobserve(item.target);
+        }
+      });
+    }, imgOptions);
+    imagesToLoad.forEach((img) => {
+      imgObserver.observe(img);
+    });
+  } else {
     imagesToLoad.forEach((img) => {
       imgObserver.observe(img);
     });
   }
-  else{
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
+}
