6200c2673bea53b00524bee62508365e7434fd54
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-11-03 12:25:02 -0500

    commit

--- Lesson 8/submit.js
+++ Lesson 8/submit.js
@@ -1,10 +1,10 @@
-
 var slider = document.getElementById("rating");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
-slider.oninput = function() {
-  output.innerHTML = this.value}
+slider.oninput = function () {
+    output.innerHTML = this.value
+}
 
 function formSubmit() {
     document.forms["myForm"].submit();
