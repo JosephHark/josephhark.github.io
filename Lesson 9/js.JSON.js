d97ea5b0eb0f2bb14df5099de8501148501d95b2
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-11-13 12:38:04 -0500

    commit

--- Lesson 9/js.JSON.js
+++ Lesson 9/js.JSON.js
@@ -1,33 +1,47 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 fetch(requestURL)
 .then(function (response) {
-return response.json();
+    return response.json();
 })
 .then(function (jsonObject) {
-console.table(jsonObject);
-for (let i = 0; i < 4; i++) {
-let card = document.createElement('section');
-let h2 = document.createElement('h2');
-let h4 = document.createElement('h4');
-let img = document.createElement("img");
-let p = document.createElement('p');
+   // console.table(jsonObject); 
 
-h2.textContent = towns[i].name;
-h4.textContent = towns[i].motto;
-img.setAttribute('src', towns[i].photo);
-p.textContent = towns[i].yearFounded;
-p.textContent = towns[i].currentPopulation;
-p.textContent = towns[i].averageRainfall;
+    const towns = jsonObject['towns'];
+    const wantedTowns = towns.filter(town =>(town.name =="Fish Haven"))
+    for (let i = 0; i < 3; i++) {
+
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let h4 = document.createElement('h4');
+        let image = document.createElement("img");
+        let p = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+
+        h2.textContent = towns[i].name;
+        h4.textContent = towns[i].motto;
+        p.textContent = 'Year Founded: ' + towns[i].yearFounded;
+        p2.textContent = 'Current Population:' + towns[i].currentPopulation;
+        p3.textContent ='Average Yearly Rainfall:' + towns[i].averageRainfall;
+        
+        
+        card.appendChild(h2);
+        card.appendChild(h4);
+        card.appendChild(p);
+        card.appendChild(p2);
+        card.appendChild(p3);
 
 
-card.appendChild(h2);
-card.appendChild(h4);
-card.appendChild(img);
-card.appendChild(p);
+    image.setAttribute('src' , towns[i].photo);
+    image.setAttribute('alt' , towns[i].name);
+    card.appendChild(image);
+
+    document.querySelector('div.cards').appendChild(card);
 
 
-document.querySelector('div.cards').appendChild(card);
-} // temporary checking for valid response and data parsing
+
+    };
+
+    
 });
-const towns = jsonObject['towns'];
