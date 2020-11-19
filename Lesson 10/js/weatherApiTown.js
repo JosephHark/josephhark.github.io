32f22b6d13bbe6aca4aa17907071010c99e6ee45
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-11-19 17:06:33 -0500

    c

--- Lesson 10/js/weatherApiTown.js
+++ Lesson 10/js/weatherApiTown.js
@@ -3,12 +3,34 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        const curtemp = document.querySelector('#current-temp');
+        curtemp.innerHTML = jsObject.main.temp;
         document.getElementById('humidity').textContent = jsObject.main.humidity;
-        document.getElementById('wind_speed').textContent = jsObject.wind.speed;
-        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
-        const desc = jsObject.weather[0].description; // note how we reference the weather array
-        document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
-        document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
-        document.getElementById('icon').setAttribute('alt', desc);
+        document.getElementById('speed').textContent = jsObject.wind.speed;
+        document.getElementById('max').textContent = jsObject.main.temp_max;
+        document.getElementById('min').textContent = jsObject.main.temp_min;
     });
+/*Calculates windchill*/
+function buildWC(t, speed) {
+
+    let wc = 35.74 + (0.6215 * t) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * t * Math.pow(speed, 0.16));
+    wc = Math.floor(wc);
+    //compute
+
+    //if windchill is greater than temp return windchill
+    return wc;
+}
+
+function getSummary() {
+    let temp = ;
+    let wind = ;
+    let windchill = "N/A"
+    if (temp <= 50 && wind > 3.0) {
+        windchill = buildWC(temp, wind);
+    }
+
+    document.getElementById('temp').innerHTML = ` ${temp}&#8457`
+    document.getElementById('speed').innerHTML = ` ${wind} MPH`
+    document.getElementById('windchill').innerHTML = `${windchill}&#8457`
+}
+getSummary();
