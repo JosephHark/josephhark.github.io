02844dde4abfde3621acc631dbc9715588fa31be
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-11-20 15:53:20 -0500

    c

--- lesson 11/js/picnic.js
+++ lesson 11/js/picnic.js
@@ -1,5 +1,5 @@
 const picnic = new Date();
-let message = document.querySelector(".message");
+let message = document.querySelector("#banner");
 
 if (picnic.getDay() == 5) {
     message.style.display = "block";
