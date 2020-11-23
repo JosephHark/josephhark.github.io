289acbdbff9e410581bb403d0fa68deddf5cc26b
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-11-23 12:28:38 -0500

    c

--- lesson 11/js/preston-Summary.js
+++ lesson 11/js/preston-Summary.js
@@ -1,19 +1,21 @@
 const prestonapiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=a21b3a033736d7d3908ca1bc674e2551&units=imperial"
-fetch(prestonapiURL)
+fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    
-    let t = parseFloat(jsObject.main.temp);
-    let s = parseFloat(jsObject.wind.speed);
-    let output = "N/A";
-    document.getElementById('weather').innerHTML = jsObject.weather[0].description;
-    document.getElementById('current-temp').innerHTML = Math.round(t) + "&#8457;";
-    if (t <= 50 && s >= 3) {
-      let f = (35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * (t * (Math.pow(s, 0.16))));
-      output = Math.round(f);
-    };
-    document.getElementById("output").innerHTML = output + "&#8457;";
-    document.getElementById('humidity').innerHTML = jsObject.main.humidity + "&#37;";
-    document.getElementById('speed').innerHTML = Math.round(s) + " mph";
-
+    console.log(jsObject);
+    var currently = document.getElementById('currently').textContent = jsObject.weather[0].main;
+    var tempF = jsObject.main.temp_max;
+    document.getElementById('tempF').innerHTML = tempF
+    var speed = jsObject.wind.speed;
+    document.getElementById('speed').innerHTML = speed
+    document.getElementById('humudity').innerHTML = jsObject.main.humidity;
+    document.getElementById('windchill').innerHTML = windchill(tempF, speed);
+    function windchill(t, s) {
+      if (t <= 50 && s >= 3.8) {
+        return Math.round(35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16) + " &deg;F";
+      }
+      else {
+        return "N/A";
+      }
+    }
   });
