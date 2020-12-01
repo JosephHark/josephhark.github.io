7c4801430936a5a63f43e46ac4fbd196b6207418
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-12-01 14:29:57 -0500

    c

---
+++ lesson 12/js/submit.js
@@ -0,0 +1,12 @@
+var slider = document.getElementById("peroid");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value
+}
+
+function formSubmit() {
+    document.forms["myForm"].submit();
+    location.replace("https://josephhark.github.io/Lesson%208/thanks.html")
+}
