b397d69a38c84d5232d58e6a6b4c1acba156b52d
JosephHark <71291466+JosephHark@users.noreply.github.com>
2020-12-02 10:55:01 -0500

    c

--- lesson 11/js/submit.js
+++ lesson 11/js/submit.js
@@ -6,7 +6,17 @@ slider.oninput = function () {
     output.innerHTML = this.value
 }
 
+document.querySelectorAll('#contact input')
+
+let whole = Array.from(document.querySelectorAll('#contact input')).reduce((acc, input) => ({
+...acc,
+[input.id]: input.value
+}), {});
+
+console.log(whole);
+/*
 function formSubmit() {
     document.forms["myForm"].submit();
-    location.replace("https://josephhark.github.io/Lesson%208/thanks.html")
+    location.replace("https://josephhark.github.io/lesson%2012/deposit.html")
 }
+*/
